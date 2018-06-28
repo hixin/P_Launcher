b530f808fdb98e75a7751debfd113f66319c26a5
AOSP STUDIO <aospstudio@gmail.com>
2018-06-28 12:20:12 +0300

    Add files via upload

---
+++ src/com/google/android/apps/nexuslauncher/PLauncherTheme.java
@@ -0,0 +1,15 @@
+package com.google.android.apps.nexuslauncher;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+
+@SuppressLint("Registered")
+public class PLauncherTheme extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+    }
+}
